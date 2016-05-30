159484f91362a71fa63505bfe6cfec616d1ef4c5
lucianocjesus <lucianocjesus@gmail.com>
2016-05-30 18:04:52 -0300

    Adicionando projeto

---
+++ wsCalculadora/wsCalculadora.Services/Calculadora.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Calculadora.asmx.cs" Class="wsCalculadora.Services.Calculadora" %>
---
+++ wsCalculadora/wsCalculadora.Services/Calculadora.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.Services;
+
+namespace wsCalculadora.Services
+{
+    /// <summary>
+    /// Summary description for Calculadora
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Calculadora : WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public Int32 Somar(Int32 valor1, Int32 valor2)
+        {
+            return valor1 + valor2;
+        }
+    }
+}
